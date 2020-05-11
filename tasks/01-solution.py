a68d6b887d4d14f1092a18fea393428532804ad3
Meggan G <gitlab@meggan.green>
2020-05-11 17:34:25 +0000

    Added doctests to calc_fuel

--- tasks/01-solution.py
+++ tasks/01-solution.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import common
 
 """ Part 1 """
@@ -25,19 +26,27 @@ def calc_fuel_requirement_addl():
     return total
 
 
+>>>>>>> 2ab6bf9e6b1e4adb85d81efffc20619ce1cc91e2
 def calc_fuel(mass):
     """ Recursive function to get the fuel needed for the mass of the fuel needed.
 
         A module of mass 14 requires 2 fuel. This fuel requires no further fuel
         (2 divided by 3 and rounded down is 0, which would call for a negative
         fuel), so the total fuel required is still just 2.
+        >>> calc_fuel(14)
+        2
+
         At first, a module of mass 1969 requires 654 fuel. Then, this fuel
-        requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel,
+        requires 216 more fuel (654 / 3 - 2); 216 then requires 70 more fuel,
         which requires 21 fuel, which requires 5 fuel, which requires no further
-        fuel. So, the total fuel required for a module of
-        mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+        fuel. The total for mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+        >>> calc_fuel(1969)
+        966
+
         The fuel required by a module of mass 100756 and its fuel is:
         33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+        >>> calc_fuel(100756)
+        50346
     """
 
     if mass < 9:
@@ -47,3 +56,8 @@ def calc_fuel(mass):
     fuel += calc_fuel(fuel)
 
     return fuel
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
