0015df0b3e43f926bef7ce9986ff51cc4631551a
Meggan G <gitlab@meggan.green>
2020-06-09 02:56:03 +0000

    FIXED None output - self.index needs to persist
    I am assuming that because the computers get run multiple times, it is necessary
    to hold the program index on entry and exit.

--- tasks/computer.py
+++ tasks/computer.py
@@ -2,86 +2,101 @@ class Computer:
     """ An Intcode Computer. """
 
     def __init__(self):
-        self.opcodes = [99, 1, 2, 3, 4, 5, 6, 7, 8]
-        # self.system  = system  #
+        self.opcodes = [99, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.inputs  = []
         self.program = None
 
 
-    def run_program(self, program, noun=None, verb=None, inputs=[]):
+    def __repr__(self):
+        return f"<Comp inputs: {self.inputs}; >"
+
+
+    def load_program(self, program, noun=None, verb=None, system=None):
+        """ Performs the setup operations. """
+
+        self.program = program
+        self.index = 0
+        if noun is not None:
+            self.program[1] = noun
+        if verb is not None:
+            self.program[2] = verb
+        if system is not None:
+            self.inputs.append(system)
+
+
+    def run_program(self, inputs):
         """ Runs the opcodes. """
 
-        # Perform setup opertions
-        self.program = program
-        # self._set_system()
-        if noun:
-            self.program[1] = noun
-        if verb:
-            self.program[2] = verb
+        self.inputs += inputs
 
         # Run each opcode
-        i, j = 0, 0  # i is the program index; j is the inputs index
-        while i < len(program):
-            opcode = self._get_opcode(i)
-            # exit early
+        while self.index < len(self.program):
+            opcode = self._get_opcode()
+
             if opcode == 99:
-                self.program = None
-                print("End of Line")
-                return output
+                # self.program = None
+                # print("End of Line")
+                return None
 
-            params = self._get_exe_params(i)
+            params = self._get_exe_params(opcode)
 
+            # Addition
             if opcode == 1:
                 self.program[params[2]] = self.program[params[0]] + self.program[params[1]]
-                i += 4
+                self.index += 4
 
+            # Multiplication
             elif opcode == 2:
                 self.program[params[2]] = self.program[params[0]] * self.program[params[1]]
-                i += 4
+                self.index += 4
 
+            # Input
             elif opcode == 3:
                 if inputs:
-                    self.program[params[0]] = inputs[j]
-                    j += 1
+                    self.program[params[0]] = self.inputs.pop(0)
                 else:
                     self._do_input(params)
-                i += 2
+                self.index += 2
 
+            # Output
             elif opcode == 4:
-                # import pdb; pdb.set_trace()
-                # print("\nInstruction:", i)
                 output = self.program[params[0]]
-                # print("Value:", output)
+                # print(f"Opcode 4 outputting {output}")
+                self.index += 2
                 return output
-                i += 2
                 
+            # Jump index not equal to zero ("if true")
             elif opcode == 5:
                 if self.program[params[0]] != 0:
-                    i = self.program[params[1]]
+                    self.index = self.program[params[1]]
                 else:
-                    i += 3
+                    self.index += 3
                 
+            # Jump index equal to zero ("if false")
             elif opcode == 6:
                 if self.program[params[0]] == 0:
-                    i = self.program[params[1]]
+                    self.index = self.program[params[1]]
                 else:
-                    i += 3
+                    self.index += 3
 
+            # Is less than -- store 1 or 0
             elif opcode == 7:
                 if self.program[params[0]] < self.program[params[1]]:
                     self.program[params[2]] = 1
                 else:
                     self.program[params[2]] = 0
-                i += 4     
+                self.index += 4     
 
+            # Equals -- store 1 or 0
             elif opcode == 8:
                 if self.program[params[0]] == self.program[params[1]]:
                     self.program[params[2]] = 1
                 else:
                     self.program[params[2]] = 0
-                i += 4
-            
-            else:
-                return "err: ", i
+                self.index += 4
+
+            # Jump relative base
+            # elif opcode == 9:
 
         return
 
@@ -99,10 +114,10 @@ class Computer:
         # TODO: Add try/except for int(value)
         value = int(value)
 
-        self.program[params[0]] = value  # self.system
+        self.program[params[0]] = value  # system
 
 
-    def _get_opcode(self, i):
+    def _get_opcode(self):
         """ Returns opcode.
         
         Arguments:
@@ -113,12 +128,12 @@ class Computer:
 
         """
 
-        opcode = int(str(self.program[i])[-2:])
+        opcode = int(str(self.program[self.index])[-2:])
         return 99 if opcode not in self.opcodes else opcode
 
 
-    def _get_exe_params(self, i):
-        """ Make proper parameters based on parameter mode.
+    def _get_exe_params(self, opcode):
+        """ Makes proper parameters based on parameter mode.
 
         Arguments:
             i {integer} -- Index of the command in `program`
@@ -128,13 +143,17 @@ class Computer:
 
         """
 
-        pmodes = [int(n) for n in list(str(self.program[i])[:-2])][::-1]
+        pmodes = [int(n) for n in list(str(self.program[self.index])[:-2])][::-1]
         while len(pmodes) < 3:
             pmodes.append(0)
 
-        params = []
-        params.append(self.program[i+1] if pmodes[0] == 0 else (i + 1))
-        params.append(self.program[i+2] if pmodes[1] == 0 else (i + 2))
-        params.append(self.program[i+3] if pmodes[2] == 0 else (i + 3))
+        params = [None, None, None]
+        params[0] = self.index + 1 if pmodes[0] == 1 else self.program[self.index+1]
+
+        if opcode in [1, 2, 5, 6, 7, 8]:
+            params[1] = self.index + 2 if pmodes[1] == 1 else self.program[self.index+2]
+        
+        if opcode in [1, 2, 7, 8]:
+            params[2] = self.index + 1 if pmodes[2] == 1 else self.program[self.index+3]
 
         return params
