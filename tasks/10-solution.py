c7bafaef409fc15a97d2a77bc760b8fc6fceae21
Meggan M Green <gitlab@meggan.green>
2020-06-10 20:21:54 +0000

    Made asteroid generator

--- tasks/10-solution.py
+++ tasks/10-solution.py
@@ -2,3 +2,30 @@
 
 # for one asteroid, i want to know it's line of sight to every other asteroid?
 # for one asteroid, i want to follow a line of sight and cancel out blocked asteroids?
+
+# it looks like lots of people used atan2 -- but idk what that is rn so i'm
+# looking at a ruby solution that followed my second thought: get the slope of
+# the line and cancel out other asteroids with that slope 
+
+import common
+
+def make_asteroids(graph):
+    """Makes a generator of asteroids in a graph.
+
+    Args:
+        graph (list of iterables): [description]
+
+    Yields:
+        complex: x, y coordinates represented as complex numbers; 
+                 x is real, y is imaginary
+    """
+
+    for y, row in enumerate(graph):
+        for x, char in enumerate(row):
+            if char == '#':
+                yield x + y * 1j
+
+
+##########
+if __name__ == "__main__":
+    graph = common.listify_input_file("10-input.txt")
