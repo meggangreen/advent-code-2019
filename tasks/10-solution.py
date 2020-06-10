17286119c478af5458265149810b961deceb926e
Meggan M Green <gitlab@meggan.green>
2020-06-10 20:02:41 +0000

    Started day 10

---
+++ tasks/10-solution.py
@@ -0,0 +1,4 @@
+""" Day 10 """
+
+# for one asteroid, i want to know it's line of sight to every other asteroid?
+# for one asteroid, i want to follow a line of sight and cancel out blocked asteroids?
