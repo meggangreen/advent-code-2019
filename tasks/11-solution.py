5c90f15cfefc79f15b872155b4dc113b49f0375d
Meggan M Green <gitlab@meggan.green>
2020-06-15 22:09:45 +0000

    Painter: Made _turn and _advance

--- tasks/11-solution.py
+++ tasks/11-solution.py
@@ -2,7 +2,8 @@
 
 """ Notes
 
-    - Robot is a Computer that paints
+    - Painter is a Computer that paints
+        - has its current coords (could track path, but not necessary)
         - registers and tracks panels
         - takes photos for input -- panel's current color
         - outputs (new color, turn direction)
@@ -32,13 +33,46 @@ class Panel(object):
         self.color = 0
         self.been_painted = False
 
+
     def __repr__(self):
         color = "white" if self.color = 1 else "black"
         painted = "painted" if self.been_painted = True else "not painted"
-        return f"<P @ {self.coords} - {color} - {painted}>"
+        return f"<Panel @ {self.coords} - {color} - {painted}>"
+
 
     def _be_painted(self, color):
         self.color = color
         self.been_painted = True
 
 
+class Painter(Computer):
+    
+    def __init__(self):
+        super().__init__()
+        self.coords = 0+0j
+        self.orientations = ("^", ">", "v", "<")
+        self.panels = dict()
+
+
+    def __repr__(self):
+        return f"<Painter @ {self.coords} {self.orientations[0]}>"
+
+
+    def _turn(self, direction):
+
+        if direction == 0:
+            self.orientations = self.orientations[3] + self.orientations[:3]
+        else:
+            self.orientations = self.orientations[1:] + self.orientations[0]
+
+
+    def _advance(self):
+
+        if self.orientations[0] == "^":
+            self.coords += 1j
+        elif self.orientations[0] == "v":
+            self.coords += -1j
+        elif self.orientations[0] == ">":
+            self.coords += 1
+        elif self.orientations[0] == "<":
+            self.coords += -1
