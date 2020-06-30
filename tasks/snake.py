199deacd8cda25ad2dd5135c6048843ffa058ed1
Meggan M Green <gitlab@meggan.green>
2020-06-30 23:00:55 +0000

    Added snake

--- tasks/snake.py
+++ tasks/snake.py
@@ -4,6 +4,17 @@ import curses, time
 
 def main(screen):
     curses.curs_set(0)  # Hide the cursor
+
+    snake = [(0, i) for i in reversed(range(20))]
+    
+    # Draw the snake
+    screen.addstr(*snake[0], '@')
+    for segment in snake[1:]:
+        screen.addstr(*segment, '*')
+
+    screen.refresh()
+
+
     time.sleep(1)
 
 
